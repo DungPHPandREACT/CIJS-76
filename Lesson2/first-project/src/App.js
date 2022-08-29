05776a81125655ac34c629cf8d8f6b430c45df5a
Tiến Dũng <dung2000cn@gmail.com>
2022-08-29 22:24:53 +0700

    ex workbook

--- Lesson2/first-project/src/App.js
+++ Lesson2/first-project/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import CardInfo from './CardInfo';
+import Task from './Task';
 
 const App = () => {
   const listInfo = [
@@ -16,17 +17,31 @@ const App = () => {
     },
   ];
 
+  const listTask = [
+    'Clean up bedroom',
+    'Buy some milk',
+    'Jogging',
+    'Learn React',
+    'Doing Exercises',
+  ];
+
   return (
-    <div className="container">
-      {listInfo.map((element, index) => {
-        return (
-          <CardInfo
-            key={index}
-            name={element.name}
-            age={element.age}
-            gender={element.gender}
-          />
-        );
+    // <div className="container">
+    //   {listInfo.map((element, index) => {
+    //     return (
+    //       <CardInfo
+    //         key={index}
+    //         name={element.name}
+    //         age={element.age}
+    //         gender={element.gender}
+    //       />
+    //     );
+    //   })}
+    // </div>
+    <div className="container-task">
+      <input type="text" placeholder="Enter your task here.." />
+      {listTask.map((value, index) => {
+        return <Task task={value} key={index} />;
       })}
     </div>
   );
