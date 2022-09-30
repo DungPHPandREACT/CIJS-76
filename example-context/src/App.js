f60237194e0fd93271afddb78dbc70b6c54daabf
Tiến Dũng <dung2000cn@gmail.com>
2022-09-30 20:58:08 +0700

    init context

--- example-context/src/App.js
+++ example-context/src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import NavBarPage from './layouts/NavBarPage';
 import { Button } from 'reactstrap';
 import Footer from './layouts/Footer';
 import Body from './layouts/Body/Body';
 
+export const ExampleContext = createContext();
+
 const App = () => {
   return (
-    <div className="container-page">
-      <NavBarPage />
-      <Body />
-      <Footer />
-    </div>
+    <ExampleContext.Provider value={{}}>
+      <div className="container-page">
+        <NavBarPage />
+        <Body />
+        <Footer />
+      </div>
+    </ExampleContext.Provider>
   );
 };
 
--- example-context/src/layouts/Body/Login.js
+++ example-context/src/layouts/Body/Login.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { ExampleContext } from '../../App';
 
 const Login = () => {
+  const data = useContext(ExampleContext);
+
+  const [userName, setUserName] = useState('');
+
+  const handleChangeUserName = (event) => {
+    setUserName(event.target.value);
+  };
+
   return (
     <div className="container-login">
       <p className="header-login">Đăng nhập</p>
       <div className="body-login">
         <div className="input-username">
-          <input type="text" placeholder="Enter your username..." />
+          <input
+            value={userName}
+            onChange={handleChangeUserName}
+            type="text"
+            placeholder="Enter your username..."
+          />
         </div>
         <button>Đăng nhập</button>
       </div>
